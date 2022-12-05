e59bdd36f983173ddc1b9ce6d83fcb0cb66fd64a
Samer Kayali <samerkkr@gmail.com>
2022-12-05 16:00:16 -0300

    feat: changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const path = require("path");
-
+const dotEnv = require("dotenv")
+const DefinePlugin = require("webpack").DefinePlugin;
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
     publicPath: process.env.PUBLIC_PATH,
-    path: path.resolve(__dirname, "../build"),
+    path: path.resolve(__dirname, "./build"),
+    filename: "static/js/bundle.[contenthash:8].js",
+    chunkFilename: "static/js/[name].chunk.js",
+    assetModuleFilename: "static/media/[name].[hash][ext]",
+    devtoolModuleFilenameTemplate: (info) =>
+      path.resolve(info.absoluteResourcePath).replace(/\\/g, "/"),
+    clean: true,
   },
 
   resolve: {
@@ -42,11 +49,14 @@ module.exports = {
   },
 
   plugins: [
+    new DefinePlugin({
+      "process.env": JSON.stringify(dotEnv.config().parsed),
+    }),
     new ModuleFederationPlugin({
       name: "host",
       filename: "remoteEntry.js",
       remotes: {
-        orbit: `orbit@@${process.env.ORBIT_PATH}remoteEntry.js`,
+        orbit: `orbit@${process.env.ORBIT_PATH}remoteEntry.js`,
         auth: `boilerplate@${process.env.BOILERPLATE_PATH}remoteEntry.js`,
       },
       exposes: {},
